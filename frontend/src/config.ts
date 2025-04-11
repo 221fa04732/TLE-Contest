dba3f41ebbd25094ea8375f5eeebf3b44c7c8e8a
221fa04732 <221fa04732@gmail.com>
2025-04-11 08:23:42 +0530

    production url added

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -2,11 +2,11 @@ export const config = {
     
     //         1. for production
     //      -----------------------
-    // BACKEND_URL : "https://tle-contest-backend.onrender.com"
+    BACKEND_URL : "https://tle-contest-backend.onrender.com"
 
 
     //        2. for local development
     //     ------------------------------
-    BACKEND_URL : "http://localhost:3000"
+    // BACKEND_URL : "http://localhost:3000"
 
 }
