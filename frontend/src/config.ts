540a595dbefca9bc051ba452fc4e1b9d36eb68ea
221fa04732 <221fa04732@gmail.com>
2025-04-18 14:03:09 +0530

    backend uri updated

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -2,7 +2,7 @@ export const config = {
     
     //         1. for production
     //      -----------------------
-    BACKEND_URL : "https://tle-contest-backend.onrender.com"
+    BACKEND_URL : "https://tle-backend-gq7j.onrender.com"
 
 
     //        2. for local development
