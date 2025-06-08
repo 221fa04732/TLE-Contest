d6b2882fd747c8bb0ebfe620aeb1818b94fe8dfc
221fa04732 <221fa04732@gmail.com>
2025-06-09 00:15:22 +0530

    prod url push

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -2,11 +2,11 @@ export const config = {
     
     //         1. for production
     //      -----------------------
-    // BACKEND_URL : "https://tle-backend-gq7j.onrender.com"
+    BACKEND_URL : "https://tle-backend-gq7j.onrender.com"
 
 
     //        2. for local development
     //     ------------------------------
-    BACKEND_URL : "http://localhost:3000"
+    // BACKEND_URL : "http://localhost:3000"
 
 }   
